53b29b7ee99c36642d832ddeeeb795a371ee83ed
Shoxer90 <shoxer90@mail.ru>
2022-04-12 10:14:03 +0400

    create Masonry method(in process)

--- index.js
+++ index.js
@@ -1,70 +1,107 @@
-let masonry = document.getElementsByClassName("masonry")
-let imagesArr = Array.from(this.document.querySelectorAll('li'))
-function building(masonry,columnWidth){
-    const columnsQuantity = Math.floor(document.body.clientWidth/columnWidth);
-        for(let i = 0; i < columnsQuantity; i++){
+// function buildingMasonry(container,columnWidth){
+//     let masonry = document.querySelector(container)
+//     let imagesArr = Array.from(this.document.querySelectorAll('li'))
+//     let columnsHeight=[]
+//     const columnsQuantity = Math.floor(document.body.clientWidth/columnWidth);
+//         for(let i=0; i < columnsQuantity; i++){
+//             let columnDiv = document.createElement('div')
+//             columnDiv.className = 'columnDiv'
+//             masonry.prepend(columnDiv)
+//             columnDiv.style.width = `${columnWidth}px`
+//             columnDiv.style.top = `5px`
+//             columnDiv.style.left = `${i*columnWidth}px`
+//         }
+//         let columnsArr = Array.from(document.getElementsByClassName('columnDiv'))
+//         setTimeout(()=>{
+//             for(let i = 0; i < imagesArr.length; i++){
+//                 columnsHeight = columnsArr.map((column) =>{
+//                     return column.offsetHeight
+//                 })
+//                currentColumn = columnsArr.find((element) => element.offsetHeight===Math.min(...columnsHeight))       
+//                      currentColumn.append(imagesArr[i])
+//              }
+//         },500)
+// }
+// building(".masonry",200)
+
+
+class Masonry{
+    constructor(container, {columnWidth,autoResize}){
+        this.container = container
+        this.columnWidth = columnWidth
+        this.autoResize = autoResize
+    }
+    debagger
+    
+    render(){
+    window.addEventListener('resize', function(){
+        let masonry = Array.from(this.document.querySelectorAll(this.container))
+        console.log(masonry)
+        let imagesArr = Array.from(this.document.querySelectorAll('li'))
+        console.log(masonry, 'masonry')
+        let columnsHeight=[]
+        const columnsQuantity = Math.floor(this.document.body.clientWidth/this.columnWidth);
+        console.log(columnsQuantity, 'quant')
+
+        for(let i=0; i < columnsQuantity; i++){
             let columnDiv = document.createElement('div')
             columnDiv.className = 'columnDiv'
-            masonry[0].prepend(columnDiv)
+            masonry.prepend(columnDiv)
+            columnDiv.style.width = `${this.columnWidth}px`
             columnDiv.style.top = `5px`
-            columnDiv.style.left = `${i*columnWidth}px`
-            console.log(document.getElementsByClassName('columnDiv')[0], "nthColumnDiv")
-            columnDiv.append(imagesArr[i])
+            columnDiv.style.left = `${i*this.columnWidth}px`
         }
         let columnsArr = Array.from(document.getElementsByClassName('columnDiv'))
-        let columnsHeight=[]
         setTimeout(()=>{
-            for(let i=4;i<imagesArr.length;i++){
+            for(let i = 0; i < imagesArr.length; i++){
                 columnsHeight = columnsArr.map((column) =>{
-                    return (column.offsetHeight)
+                    return column.offsetHeight
                 })
-               currentColumn = columnsArr.find((element) => element.offsetHeight===Math.min(...columnsHeight))       
-                  console.log(currentColumn)
-                     currentColumn.append(imagesArr[i])
-             }
-            console.log(columnsHeight ,"heights of columns")
-        },1000)
+            let currentColumn = columnsArr.find((element) => element.offsetHeight===Math.min(...columnsHeight))       
+                    currentColumn.append(imagesArr[i])
+            }
+        },500)
+        },true)
+    }
 }
-building(masonry,200)
+
+
+let build = new Masonry('.masonry', {
+    columnWidth: 200,
+    autoResize: true
+})
+// console.log(Masonry.prototype.constructor)
+
+console.log(build.container)
+build.render()
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// class Masonry{
-//     constructor(columnWidth,autoResize){
-//         this.columnWidth = columnWidth
-//         this.autoResize = autoResize
-//     }
-//     render(){
-//         if(this.autoResize === true){
-//             window.addEventListener('resize', function(){
-//                 const columnsQuantity = Math.floor(
-//                     document.body.clientWidth/this.columnWidth
-//                 );
-//                 for(let i = 0; i < columnsQuantity; i++){
-//                     let columnDiv = document.createElement('div')
-//                     columnDiv.className = 'columnDiv'
-//                     columnDiv.style.height = "fit content"
-//                     masonry[0].prepend(columnDiv)
-//                     columnDiv.prepend(imagesArr[i])
-//                 }
-//             }, true)
+// function building(container,columnWidth){
+//     let masonry = document.querySelector(container)
+//     console.log(masonry)
+//     let imagesArr = Array.from(this.document.querySelectorAll('li'))
+//     let columnsHeight=[]
+//     const columnsQuantity = Math.floor(document.body.clientWidth/columnWidth);
+//         for(let i=0; i < columnsQuantity; i++){
+//             let columnDiv = document.createElement('div')
+//             columnDiv.className = 'columnDiv'
+//             masonry.prepend(columnDiv)
+//             columnDiv.style.width = `${columnWidth}px`
+//             columnDiv.style.top = `5px`
+//             columnDiv.style.left = `${i*columnWidth}px`
 //         }
-//     }
+//         let columnsArr = Array.from(document.getElementsByClassName('columnDiv'))
+//         setTimeout(()=>{
+//             for(let i = 0; i < imagesArr.length; i++){
+//                 columnsHeight = columnsArr.map((column) =>{
+//                     return column.offsetHeight
+//                 })
+//                currentColumn = columnsArr.find((element) => element.offsetHeight===Math.min(...columnsHeight))       
+//                      currentColumn.append(imagesArr[i])
+//              }
+//         },500)
 // }
-// let build = new Masonry('.masonry', {
-//     columnWidth: 200,
-//     autoResize: true
-// })
+// building(".masonry",200)
--- masonry.js
+++ masonry.js
@@ -1,35 +1,49 @@
-let masonry = document.getElementsByClassName("masonry")
-let imagesArr = Array.from(this.document.querySelectorAll('li'))
 
-// JS variables
-let windowWidth = document.body.clientWidth;
-// console.log("windowWidth", windowWidth)
-let columnWidth = 200
-// let arrOfColumns=[Math.floor(windowWidth/columnWidth)]
-// console.log(imagesArr[30].offsetWidth)
-const columnsQuantity = Math.floor(windowWidth/columnWidth);
-function building(){
-    for(let i = 0; i < columnsQuantity; i++){
-        let columnDiv = document.createElement('div')
-        columnDiv.className = 'columnDiv'
-        columnDiv.style.height = "fit content"
-        masonry[0].prepend(columnDiv)
-        columnDiv.prepend(imagesArr[i])
+class Masonry{
+    constructor(container,{columnWidth,autoResize}){
+        this.container = container
+        this.columnWidth = columnWidth
+        this.autoResize = autoResize
     }
+    render(){
+        let fixedColumnsNumber=0
+        let columnsArr =""
+        // if(this.autoResize === true){
+            // window.addEventListener('resize', function(){
+                let columnsQuantity = Math.floor(document.body?.clientWidth / this.columnWidth);
+                let imagesArr = document.querySelectorAll('li')
+                if(columnsQuantity===fixedColumnsNumber) return
+                    for(let i=0;i<fixedColumnsNumber;i++){
+                        columnsArr[i]?.replaceWith(...columnsArr[i]?.childNodes)
+                    }
+                    for(let i=0; i < columnsQuantity; i++){
+                        let columnDiv = document.createElement('div')
+                        columnDiv.className = 'columnDiv'
+                        document.querySelector(this.container).prepend(columnDiv)
+                        columnDiv.style.width = `${this.columnWidth}px`
+                        columnDiv.style.left = `${i*this.columnWidth}px`
+                    }
+                fixedColumnsNumber = columnsQuantity
+                    columnsArr = Array.from(document.getElementsByClassName('columnDiv'))
+                for(let i = 0; i < imagesArr.length; i++){
+                    let columnsHeight = columnsArr.map(column => column.offsetHeight)
+                    let currentColumn = columnsArr?.find((element) => element?.offsetHeight===Math.min(...columnsHeight))       
+                            currentColumn?.append(imagesArr[i])
+                    }
+            // })
+    //    }
+    }
+    divMaker(){
+
+    }
+
 }
-building(windowWidth,columnWidth)
 
-window.addEventListener('resize', function(){
-//  currentColumnsQuantity =  document.body.clientWidth/
-console.log(windowWidth)     //constant a
-console.log(document.body.clientWidth)   //variaBLE
-    // building(windowWidth,columnWidth)
-    if(Math.floor(windowWidth/columnWidth)+1 === columnsQuantity){
-        columnsQuantity+=1
-        // arrOfColumns.push(Math.floor(windowWidth/columnWidth))
-        building()
 
-       console.log(columnsQuantity , "columns")
 
-    }
-}, true)
+let build = new Masonry('.masonry', {
+    columnWidth: 200,
+    autoResize: true
+})
+build.render()
+console.log( typeof build.columnWidth)
