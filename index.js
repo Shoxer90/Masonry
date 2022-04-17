3bd216e8b42b538a125a47cdde855ba70344938f
Shoxer90 <shoxer90@mail.ru>
2022-04-17 19:53:30 +0400

    compacting code

--- index.js
+++ index.js
@@ -2,40 +2,32 @@ function Masonry(container,{columnWidth,autoResize}) {
     this.container = container
     this.columnWidth = columnWidth
     this.autoResize = autoResize
-}
-Masonry.prototype.render = function() { 
     window.addEventListener( 'load' ,() => {
-        let baseFunc = ()=>{
-            if(window.innerWidth < this.columnWidth)return
-            let masonry = document.querySelector(this.container)
-            masonry.style.position = `relative`
-            let images =Array.from(masonry.querySelectorAll('img'))
-            let arrTop = []
-            let minTopPosition = 0
-            for(let i = 0 ; i<images.length; i++) {
-                images[i].style.width = `${this.columnWidth}px`
-                images[i].style.position = `absolute`
-                images[i].style.top = `0px`
-                if( i*this.columnWidth + this.columnWidth < window.innerWidth ) {
-                    images[i].style.left = `${i*this.columnWidth}px`
-                    arrTop.push(images[i].offsetTop+(images[i].height))
-                }else{
-                    images[i].style.left =  `${minTopPosition*this.columnWidth}px`
-                    images[i].style.top =`${arrTop[minTopPosition]}px`
-                    arrTop[minTopPosition] = images[i].height+arrTop[minTopPosition]
-                }
-                minTopPosition = arrTop.indexOf(Math.min(...arrTop))
-            }
-        }
-       if(this.autoResize === true) {
-       window.addEventListener( 'resize' ,() => {
-            return baseFunc()
-       })
-        }
-          return baseFunc()
-    })
+        return  this.render();
+  });
+    if(autoResize) {
+        window.addEventListener( 'resize' ,() => {
+             return this.render();
+       });
+    }
 }
 
+Masonry.prototype.render = function() { 
+    let masonry = document.querySelector(this.container)
+    let images = masonry.querySelectorAll('img');
+    let arrTop = [0]
+    let minTopPosition = 0
+    for(let j=0 ;j<images.length; j++){
+        if(j<window.innerWidth/this.columnWidth - 1){
+            arrTop[j] = 0
+        }
+        minTopPosition = arrTop.indexOf(Math.min(...arrTop))
+        images[j].style.width = `${this.columnWidth}px`;
+        images[j].style.top= arrTop[minTopPosition]+'px'
+        images[j].style.left= minTopPosition*this.columnWidth+'px'
+        arrTop[minTopPosition]+=images[j].height
+    }
+}
  let build = new Masonry('.masonry', {
         columnWidth:200,
         autoResize: true
